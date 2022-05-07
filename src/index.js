4ec12a452e478845b5c71d795ed37e545dc309e7
man lass <lassanaa14@gmail.com>
2022-05-07 16:36:42 -0600

    fix linters

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import './assets/images/trash-can.png';
 import userActionHandler from './modules/userActions.js';
 import { initStorage } from './modules/localStorage.js';
 import taskStatus from './modules/taskStatus.js';
-import butonHandler from './modules/clearAll.js';
 
-butonHandler;
 userActionHandler();
 window.addEventListener('load', initStorage);
 window.addEventListener('load', taskStatus);
+const button = document.getElementById('button-container');
+button.addEventListener('click', () => {
+  const list = JSON.parse(localStorage.getItem('Tasks'));
+  const result = list.filter((element) => element.completed !== true);
+  localStorage.setItem('Tasks', JSON.stringify(result));
+});
--- src/modules/clearAll.js
+++ src/modules/clearAll.js
@@ -1,8 +1,8 @@
 const button = document.getElementById('button-container');
-const butonHandler = button.addEventListener('click', () => {
+const botonHandler = button.addEventListener('click', () => {
   const list = JSON.parse(localStorage.getItem('Tasks'));
   const result = list.filter((element) => element.completed !== true);
   localStorage.setItem('Tasks', JSON.stringify(result));
 });
 
-export default butonHandler;
+export default botonHandler;
